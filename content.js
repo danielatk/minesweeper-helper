436d15421402709208b430a275bd5e82ad0438d1
Daniel <danatkhoo@yahoo.com.br>
2023-02-26 11:35:34 -0300

    inputting board details in a generic way

--- content.js
+++ content.js
@@ -45,6 +45,71 @@ function getBoard() {
 
     let currentLevel = document.getElementsByClassName('level-select-link active')[0];
 
+    if (typeof currentLevel === 'undefined') {
+        let numCols = 1000;
+        let numRows = 1000;
+        let colsAdjusted = false;
+        let breakLoop = false;
+        let data = '';
+        let i;
+        for (i = 0; i < numRows; i++) {
+            if (breakLoop) {
+                break;
+            }
+            for (let j = 0; j < numCols; j++) {
+                let cell = document.getElementById(`cell_${j}_${i}`);
+                if (typeof cell === 'undefined' || cell === null) {
+                    if (colsAdjusted) {
+                        breakLoop = true;
+                        break;
+                    }
+                    numCols = j;
+                    colsAdjusted = true;
+                    break;
+                }
+                let cellStatus = 'H';
+                if (cell.className.includes('hd_flag')) {
+                    cellStatus = 'F';
+                } else if (cell.className.includes('hd_type')) {
+                    cellStatus = parseInt(cell.className.slice(-1));
+                }
+                data = data + cellStatus;
+            }
+            data = data + '\n';
+        }
+        let hundreds = document.getElementById('top_area_mines_100');
+        let tens = document.getElementById('top_area_mines_10');
+        let ones = document.getElementById('top_area_mines_1');
+        let minesStr = '';
+        let listNums = [hundreds, tens, ones];
+        for (let num of listNums) {
+            if (num.classList.contains('hd_top-area-num0')) {
+                minesStr += '0';
+            } else if (num.classList.contains('hd_top-area-num1')) {
+                minesStr += '1';
+            } else if (num.classList.contains('hd_top-area-num2')) {
+                minesStr += '2';
+            } else if (num.classList.contains('hd_top-area-num3')) {
+                minesStr += '3';
+            } else if (num.classList.contains('hd_top-area-num4')) {
+                minesStr += '4';
+            } else if (num.classList.contains('hd_top-area-num5')) {
+                minesStr += '5';
+            } else if (num.classList.contains('hd_top-area-num6')) {
+                minesStr += '6';
+            } else if (num.classList.contains('hd_top-area-num7')) {
+                minesStr += '7';
+            } else if (num.classList.contains('hd_top-area-num8')) {
+                minesStr += '8';
+            } else if (num.classList.contains('hd_top-area-num9')) {
+                minesStr += '9';
+            }
+        }
+        mines = parseInt(minesStr, 10);
+        let header = `${numCols}x${i-1}x${mines}\n`;
+        return header + data;
+    }
+
     switch (currentLevel.textContent) {
         case 'Easy':
         case 'Beginner':
